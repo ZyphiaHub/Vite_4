c847759998c2cc7a4013d5db570e566e9f9c2dff
ZyphiaHub <149183538+ZyphiaHub@users.noreply.github.com>
2024-11-06 11:34:16 +0100

    Changes to be committed: 	renamed:    docs/assets/index-D1hJOHyx.js -> docs/assets/index-Dqz71nen.js 	modified:   docs/index.html 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/vite_4/',
+  base: '/Vite_4/',
   build: {
       outDir: 'docs',
       emptyOutDir: true
